88f970118b11e5ba70d5e04808c92a10288a4451
P3lavacas <61715015+P3lavacas@users.noreply.github.com>
2020-05-23 13:27:10 -0500

    actividad 15

---
+++ js/signin.js
@@ -0,0 +1,31 @@
+window.onload = init;
+
+function init(){
+  //boton para registro
+  document.querySelector('.btn-secondary').addEventListener('click', function(){
+    window.location.href = "login.html"
+  });
+  //boton para Entrar
+  document.querySelector('.btn-primary').addEventListener('click',login);
+}
+function signin(){
+  var name = document.getElementById('input-name').value;
+  var mail = document.getElementById('input-mail').value;
+  var pass = document.getElementById('input-password').value;
+
+  axios({
+    method: 'post',
+    url: 'http://localhost:3000/user/signin',
+    data:{
+      user_name: name,
+      user_mail: mail,
+      user_password:pass
+    }
+  }).then(function(res){
+    console.log(res);
+    alert("Resgistro Exitoso");
+    window.location.href = "login.html";
+  }).catch(function(err){
+    console.log(err);
+  })
+}
